3668b0a73fc6981c3deebe499cba6066a4a8add3
ryan123rudder <ryan123rudder@gmail.com>
2023-09-14 13:45:06 -0600

    Created classes, methods, and static vars for each GamePiece. Driver: Ryan

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -1,5 +1,35 @@
 package levelPieces;
 
-public class LevelSetup {
+import java.util.ArrayList;
 
+public class LevelSetup {
+	
+	public void createLevel(int levelNum) {
+		switch(levelNum) {
+			case 0:
+				break;
+				
+			case 1:
+				break;
+		}
+	}
+	
+	public gameEngine.Drawable[] getBoard(){
+		return null;
+		
+	}
+	
+	public ArrayList<gameEngine.Moveable> getMovingPieces(){
+		return null;
+		
+	}
+	
+	public ArrayList<GamePiece> getIntereactingPieces(){
+		return null;
+		
+	}
+	
+	public int getPlayerStartLoc() {
+		return 0;
+	}
 }
