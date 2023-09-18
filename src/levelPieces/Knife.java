5e8e055f9fb76bd61536ebf473055870a8c2799e
hannahmillxr <94191025+hannahmillxr@users.noreply.github.com>
2023-09-17 21:41:40 -0600

    Update Knife.java
    Added interaction result to knife class

--- src/levelPieces/Knife.java
+++ src/levelPieces/Knife.java
@@ -7,15 +7,22 @@ public class Knife extends GamePiece implements Drawable {
 
 	private static char symbol = '/';
 	private static String label = "Knife";
+	private int location;
 	public Knife(int location) {
 		super(symbol, label, location);
+		this.location = location;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
 		// TODO Auto-generated method stub
-		return null;
+		int currPos = this.getLocation();
+		if (currPos == playerLocation) {
+			return InteractionResult.GET_POINT;
+		}
+		return InteractionResult.NONE;
+
 	}
 
 }
